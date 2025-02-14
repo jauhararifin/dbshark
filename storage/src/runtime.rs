2d8928d0f9ebc42bb67c32c59aa954664e235854
Jauhar Arifin <jauhararifin10@gmail.com>
2025-02-14 17:17:28 +0700

    add file, join and atomic for new tokio runtime

--- storage/src/runtime.rs
+++ storage/src/runtime.rs
@@ -1,8 +1,25 @@
+use std::io;
 use std::ops::{Deref, DerefMut};
+use std::path::Path;
 
 pub trait Runtime: 'static {
     type Mutex<T: Send + Sync>: Mutex<T>;
     type RwMutex<T: Send + Sync>: RwMutex<T>;
+
+    type JoinHandle: JoinHandle;
+
+    type File: File;
+
+    type AtomicUsize: Atomic<usize>;
+    type AtomicU8: Atomic<u8>;
+    type AtomicU16: Atomic<u16>;
+    type AtomicU32: Atomic<u32>;
+    type AtomicU64: Atomic<u64>;
+    type AtomicIsize: Atomic<isize>;
+    type AtomicI8: Atomic<i8>;
+    type AtomicI16: Atomic<i16>;
+    type AtomicI32: Atomic<i32>;
+    type AtomicI64: Atomic<i64>;
 }
 
 pub trait Mutex<T: Send + Sync>: Send + Sync {
@@ -38,4 +55,35 @@ pub trait RwMutex<T: Send + Sync>: Send + Sync {
 
 pub trait RwMutexReadGuard<'a, T> {
     async fn unlock(self);
+}
+
+pub trait JoinHandle {
+    async fn join(self);
+}
+
+pub trait File: Sized + Send + Sync {
+    async fn open(path: impl AsRef<Path>) -> io::Result<Self>;
+
+    async fn is_file(&self) -> io::Result<bool>;
+
+    async fn len(&self) -> io::Result<u64>;
+
+    async fn seek(&mut self, position: io::SeekFrom) -> io::Result<()>;
+
+    async fn read(&mut self, buff: &mut [u8]) -> io::Result<usize>;
+
+    async fn read_exact(&mut self, buff: &mut [u8]) -> io::Result<()>;
+
+    async fn write_all(&mut self, buff: &[u8]) -> io::Result<()>;
+
+    async fn sync(&mut self) -> io::Result<()>;
+
+    async fn truncate(&mut self, size: u64) -> io::Result<()>;
+}
+
+pub trait Atomic<T>: Send + Sync {
+    fn new(value: T) -> Self;
+    async fn load(&self) -> T;
+    async fn compare_and_exchange(&self, old: T, new: T) -> bool;
+    async fn fetch_add(&self, delta: T) -> T;
 }
--- storage/src/tokio.rs
+++ storage/src/tokio.rs
@@ -1,12 +1,28 @@
-use crate::runtime::{Mutex, MutexGuard, Runtime, RwMutex, RwMutexReadGuard};
+use crate::runtime::{File, JoinHandle, Mutex, MutexGuard, Runtime, RwMutex, RwMutexReadGuard};
 use parking_lot;
 use std::ops::{Deref, DerefMut};
+use tokio;
 
 pub struct TokioRuntime;
 
 impl Runtime for TokioRuntime {
     type Mutex<T: Send + Sync> = TokioMutex<T>;
     type RwMutex<T: Send + Sync> = TokioRwMutex<T>;
+
+    type JoinHandle = TokioJoinHandle;
+
+    type File = TokioFile;
+
+    type AtomicUsize = AtomicUsize;
+    type AtomicU8 = AtomicU8;
+    type AtomicU16 = AtomicU16;
+    type AtomicU32 = AtomicU32;
+    type AtomicU64 = AtomicU64;
+    type AtomicIsize = AtomicIsize;
+    type AtomicI8 = AtomicI8;
+    type AtomicI16 = AtomicI16;
+    type AtomicI32 = AtomicI32;
+    type AtomicI64 = AtomicI64;
 }
 
 pub struct TokioMutex<T> {
@@ -18,18 +34,21 @@ impl<T: Send + Sync> Mutex<T> for TokioMutex<T> {
     where
         Self: 'a;
 
+    #[inline]
     fn new(data: T) -> Self {
         Self {
             inner: parking_lot::Mutex::new(data),
         }
     }
 
+    #[inline]
     async fn lock(&self) -> Self::Guard<'_> {
         TokioGuard {
             inner: Some(self.inner.lock()),
         }
     }
 
+    #[inline]
     async fn try_lock(&self) -> Option<Self::Guard<'_>> {
         self.inner
             .try_lock()
@@ -44,24 +63,28 @@ pub struct TokioGuard<'a, T> {
 impl<'a, T> Deref for TokioGuard<'a, T> {
     type Target = T;
 
+    #[inline]
     fn deref(&self) -> &Self::Target {
         self.inner.as_ref().unwrap().deref()
     }
 }
 
 impl<'a, T> DerefMut for TokioGuard<'a, T> {
+    #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.inner.as_mut().unwrap().deref_mut()
     }
 }
 
 impl<'a, T: Send + Sync> MutexGuard<T> for TokioGuard<'a, T> {
+    #[inline]
     async fn unlock(mut self) {
         self.inner.take();
     }
 }
 
 impl<'a, T> Drop for TokioGuard<'a, T> {
+    #[inline]
     fn drop(&mut self) {
         if self.inner.is_none() {
             panic!("mutex guard is dropped without unlock")
@@ -81,24 +104,28 @@ impl<T: Send + Sync> RwMutex<T> for TokioRwMutex<T> {
     where
         Self: 'a;
 
+    #[inline]
     fn new(data: T) -> Self {
         Self {
             inner: Some(parking_lot::RwLock::new(data)),
         }
     }
 
+    #[inline]
     async fn read(&self) -> Self::ReadGuard<'_> {
         TokioReadGuard {
             inner: Some(self.inner.as_ref().unwrap().read()),
         }
     }
 
+    #[inline]
     async fn write(&self) -> Self::WriteGuard<'_> {
         TokioWriteGuard {
             inner: Some(self.inner.as_ref().unwrap().write()),
         }
     }
 
+    #[inline]
     async fn try_write(&self) -> Option<Self::WriteGuard<'_>> {
         let guard = self.inner.as_ref().unwrap().try_write()?;
         Some(TokioWriteGuard { inner: Some(guard) })
@@ -110,6 +137,7 @@ pub struct TokioReadGuard<'a, T> {
 }
 
 impl<'a, T> From<TokioWriteGuard<'a, T>> for TokioReadGuard<'a, T> {
+    #[inline]
     fn from(mut value: TokioWriteGuard<'a, T>) -> Self {
         Self {
             inner: Some(parking_lot::RwLockWriteGuard::downgrade(
@@ -122,18 +150,21 @@ impl<'a, T> From<TokioWriteGuard<'a, T>> for TokioReadGuard<'a, T> {
 impl<'a, T> Deref for TokioReadGuard<'a, T> {
     type Target = T;
 
+    #[inline]
     fn deref(&self) -> &Self::Target {
         self.inner.as_ref().unwrap().deref()
     }
 }
 
 impl<'a, T: Send + Sync> RwMutexReadGuard<'a, T> for TokioReadGuard<'a, T> {
+    #[inline]
     async fn unlock(mut self) {
         self.inner.take();
     }
 }
 
 impl<'a, T> Drop for TokioReadGuard<'a, T> {
+    #[inline]
     fn drop(&mut self) {
         if self.inner.is_none() {
             panic!("rwmutex read guard is dropped without unlock")
@@ -148,13 +179,144 @@ pub struct TokioWriteGuard<'a, T> {
 impl<'a, T> Deref for TokioWriteGuard<'a, T> {
     type Target = T;
 
+    #[inline]
     fn deref(&self) -> &Self::Target {
         self.inner.as_ref().unwrap().deref()
     }
 }
 
 impl<'a, T> DerefMut for TokioWriteGuard<'a, T> {
+    #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.inner.as_mut().unwrap().deref_mut()
     }
 }
+
+pub struct TokioJoinHandle(tokio::task::JoinHandle<()>);
+
+impl JoinHandle for TokioJoinHandle {
+    #[inline]
+    async fn join(self) {
+        self.0.await.expect("thread should not panic")
+    }
+}
+
+pub struct TokioFile(tokio::fs::File);
+
+impl File for TokioFile {
+    #[inline]
+    async fn open(path: impl AsRef<std::path::Path>) -> std::io::Result<Self> {
+        let f = tokio::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(false)
+            .open(path.as_ref())
+            .await?;
+        lock(&f)?;
+        Ok(Self(f))
+    }
+
+    #[inline]
+    async fn is_file(&self) -> std::io::Result<bool> {
+        Ok(self.0.metadata().await?.is_file())
+    }
+
+    #[inline]
+    async fn len(&self) -> std::io::Result<u64> {
+        Ok(self.0.metadata().await?.len())
+    }
+
+    #[inline]
+    async fn seek(&mut self, position: std::io::SeekFrom) -> std::io::Result<()> {
+        use tokio::io::AsyncSeekExt;
+        self.0.seek(position).await?;
+        Ok(())
+    }
+
+    #[inline]
+    async fn read(&mut self, buff: &mut [u8]) -> std::io::Result<usize> {
+        use tokio::io::AsyncReadExt;
+        self.0.read(buff).await
+    }
+
+    #[inline]
+    async fn read_exact(&mut self, buff: &mut [u8]) -> std::io::Result<()> {
+        use tokio::io::AsyncReadExt;
+        self.0.read_exact(buff).await?;
+        Ok(())
+    }
+
+    #[inline]
+    async fn write_all(&mut self, buff: &[u8]) -> std::io::Result<()> {
+        use tokio::io::AsyncWriteExt;
+        self.0.write_all(buff).await?;
+        Ok(())
+    }
+
+    #[inline]
+    async fn sync(&mut self) -> std::io::Result<()> {
+        self.0.sync_all().await
+    }
+
+    #[inline]
+    async fn truncate(&mut self, size: u64) -> std::io::Result<()> {
+        self.0.set_len(size).await
+    }
+}
+
+#[cfg(unix)]
+fn lock(f: &tokio::fs::File) -> std::io::Result<()> {
+    use std::os::unix::io::AsRawFd;
+    use syscalls::{syscall2, Sysno};
+    let fd = f.as_raw_fd();
+    const LOCK_EX: usize = 0x2;
+    let result = unsafe { syscall2(Sysno::flock, fd as usize, LOCK_EX) };
+    if let Err(err) = result {
+        Err(std::io::Error::from_raw_os_error(err.into_raw()))
+    } else {
+        Ok(())
+    }
+}
+
+macro_rules! impl_atomic {
+    ($name:ident, $ty:ident) => {
+        pub struct $name(atomic::$name);
+
+        impl crate::runtime::Atomic<$ty> for $name {
+            #[inline]
+            fn new(value: $ty) -> Self {
+                Self(atomic::$name::new(value))
+            }
+
+            #[inline]
+            async fn load(&self) -> $ty {
+                self.0.load(atomic::Ordering::SeqCst)
+            }
+
+            #[inline]
+            async fn compare_and_exchange(&self, old: $ty, new: $ty) -> bool {
+                self.0
+                    .compare_exchange(old, new, atomic::Ordering::SeqCst, atomic::Ordering::SeqCst)
+                    .is_ok()
+            }
+
+            #[inline]
+            async fn fetch_add(&self, delta: $ty) -> $ty {
+                self.0.fetch_add(delta, atomic::Ordering::SeqCst)
+            }
+        }
+    };
+}
+
+use std::sync::atomic;
+impl_atomic!(AtomicUsize, usize);
+impl_atomic!(AtomicU8, u8);
+impl_atomic!(AtomicU16, u16);
+impl_atomic!(AtomicU32, u32);
+impl_atomic!(AtomicU64, u64);
+impl_atomic!(AtomicIsize, isize);
+impl_atomic!(AtomicI8, i8);
+impl_atomic!(AtomicI16, i16);
+impl_atomic!(AtomicI32, i32);
+impl_atomic!(AtomicI64, i64);
