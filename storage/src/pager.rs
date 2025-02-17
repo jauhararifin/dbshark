3e369e938f49d798ba44ac68c000f9823a9f817f
Jauhar Arifin <jauhararifin10@gmail.com>
2025-02-17 11:48:00 +0700

    init pager wal and evictor

--- storage/src/pager.rs
+++ storage/src/pager.rs
@@ -1,3 +1,5 @@
+mod evictor;
+mod log;
 
 pub(crate) const MINIMUM_PAGE_SIZE: usize = 256;
 pub(crate) const MAXIMUM_PAGE_SIZE: usize = 0x4000;
---
+++ storage/src/pager/evictor.rs
@@ -0,0 +1,105 @@
+use anyhow::anyhow;
+use indexmap::IndexSet;
+
+pub(crate) struct Evictor {
+    ref_count: Vec<usize>,
+    // It is important to use IndexSet and not HashSet to make it deterministic
+    free_frames: IndexSet<usize>,
+    free_and_clean: IndexSet<usize>,
+}
+
+impl Evictor {
+    pub(crate) fn new(n: usize) -> Self {
+        Self {
+            ref_count: vec![0; n],
+            free_frames: IndexSet::default(),
+            free_and_clean: IndexSet::default(),
+        }
+    }
+
+    pub(crate) fn acquired(&mut self, frame_id: usize) {
+        log::trace!("acquired frame_id={frame_id}");
+        assert!(frame_id < self.ref_count.len());
+        self.ref_count[frame_id] += 1;
+        self.free_frames.swap_remove(&frame_id);
+        self.free_and_clean.swap_remove(&frame_id);
+    }
+
+    pub(crate) fn released(&mut self, frame_id: usize, dirty: bool) {
+        log::trace!(frame_id,dirty;"released");
+        assert!(frame_id < self.ref_count.len());
+        self.ref_count[frame_id] -= 1;
+        let free = self.ref_count[frame_id] == 0;
+        if free {
+            self.free_frames.insert(frame_id);
+        }
+
+        if dirty {
+            self.free_and_clean.swap_remove(&frame_id);
+        }
+
+        if !dirty && free {
+            self.free_and_clean.insert(frame_id);
+        }
+    }
+
+    pub(crate) fn evict_and_acquire(&mut self) -> anyhow::Result<(usize, bool)> {
+        log::trace!(
+            free_and_clean=self.free_and_clean.len(),
+            free_frames=self.free_frames.len();
+            "evict",
+        );
+
+        if let Some(frame_id) = self.free_and_clean.iter().next().copied() {
+            self.ref_count[frame_id] = 1;
+            self.free_and_clean.swap_remove(&frame_id);
+            self.free_frames.swap_remove(&frame_id);
+            Ok((frame_id, false))
+        } else if let Some(frame_id) = self.free_frames.iter().next().copied() {
+            self.ref_count[frame_id] = 1;
+            self.free_frames.swap_remove(&frame_id);
+            Ok((frame_id, true))
+        } else {
+            return Err(anyhow!("all pages are pinned"));
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_evict_free_page() {
+        let mut evictor = Evictor::new(5);
+        evictor.acquired(0);
+        evictor.acquired(1);
+        evictor.acquired(2);
+        evictor.released(2, false);
+        evictor.acquired(3);
+        evictor.acquired(4);
+        let (frame_id, evict) = evictor.evict_and_acquire().unwrap();
+        assert_eq!(2, frame_id);
+        assert!(!evict);
+        evictor.released(3, true);
+        evictor.acquired(2);
+        let (frame_id, evict) = evictor.evict_and_acquire().unwrap();
+        assert_eq!(3, frame_id);
+        assert!(evict);
+    }
+
+    #[test]
+    fn test_eviction_full() {
+        let mut evictor = Evictor::new(5);
+        evictor.acquired(0);
+        assert!(evictor.evict_and_acquire().is_err());
+        evictor.acquired(1);
+        assert!(evictor.evict_and_acquire().is_err());
+        evictor.acquired(2);
+        assert!(evictor.evict_and_acquire().is_err());
+        evictor.acquired(3);
+        assert!(evictor.evict_and_acquire().is_err());
+        evictor.acquired(4);
+        assert!(evictor.evict_and_acquire().is_err());
+    }
+}
